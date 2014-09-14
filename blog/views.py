2de24cfcc4ed40726d8f07cf697855db1465c6b6
Ravi Malik <ravimalik2364@gmail.com>
2014-09-14 17:08:48 +0530

    Created Home Page for a Blog
    Created the home page of a blog which currently shows the latest 3 posts in the
    blog. The view is triggered by passing the urls blog/id/ .

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from blog.models import Post, Blog
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
-# Create your views here.
+def home(request, blog_id):
+	context = {}
+	template = "blog/templates/home.html"
+
+	try:
+		blog_id = int(blog_id)
+
+		_blog = get_object_or_404(Blog, pk=blog_id)
+
+		posts = Post.objects.filter(blog = _blog).order_by("-published_on")[:3]
+		context["posts"] = posts
+		context["blog_name"] = _blog.name
+	except ValueError:
+		raise Http404
+
+	return render(request, template, context)
+	
