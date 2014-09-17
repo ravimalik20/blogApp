cf40a9061a493c6b67de4bf928ea3a1c30a61e6d
Ravi Malik <ravimalik2364@gmail.com>
2014-09-17 12:44:48 +0530

    Seperate Web Page for BlogPosts.
    Created seperate view for the Blog Posts. The urlpattern followed is :
    /blog/post_owner_username/blog_url/post_id/

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('blog.views',
+	url(r'(?P<post_owner>\w+)/(?P<blog_url>\w+)/(?P<post_id>\d+)/$', 'blog_post'),	
 	url(r'(?P<blog_owner>\w+)/(?P<blog_url>\w+)/$', 'home'),
+	
 )
--- blog/views.py
+++ blog/views.py
@@ -14,4 +14,18 @@ def home(request, blog_owner, blog_url):
 	context["blog_title"] = _blog.title
 
 	return render(request, template, context)
-	
+
+def blog_post(request, post_owner, blog_url, post_id):
+	errors = []
+	template = "blog/templates/post.html"
+	try:
+		post_id = int(post_id)
+		post = get_object_or_404(Post, owner__username = post_owner, blog__url = blog_url, pk=post_id)
+		if not post.visible:
+			errors.append("Post is marked invisible.")
+			post = None
+	except ValueError:
+		errors.append("Invalid post id.")
+
+	context = {"errors": errors, "post": post}
+	return render(request, template, context)
