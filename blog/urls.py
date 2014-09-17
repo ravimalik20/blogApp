c14df8ac30469bfe93d6fbacc472fff0fc9f615f
Ravi Malik <ravimalik2364@gmail.com>
2014-09-17 11:54:33 +0530

    Incorporated Namespacing in Blog URLs.
    Updated the views, templates and urlconf to incorporate namespaces in blog URLs.
    
    home view now contains two arguments namely blog_owner and blog_url. Out of
    these two, the effective blog URL is generated which is blog_owner/blog_url.
    
    home.html template also replaces blog_name with blog_title to avoid confusion.
    
    urlconf captures the two arguments for home view.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('blog.views',
-	url(r'(?P<blog_id>\d+)/$', 'home'),
+	url(r'(?P<blog_owner>\w+)/(?P<blog_url>\w+)/$', 'home'),
 )
--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,15 @@ from blog.models import Post, Blog
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 
-def home(request, blog_id):
+def home(request, blog_owner, blog_url):
 	context = {}
 	template = "blog/templates/home.html"
 
-	try:
-		blog_id = int(blog_id)
+	_blog = get_object_or_404(Blog, owner__username = blog_owner, url = blog_url)
 
-		_blog = get_object_or_404(Blog, pk=blog_id)
-
-		posts = Post.objects.filter(blog = _blog).order_by("-published_on")[:3]
-		context["posts"] = posts
-		context["blog_name"] = _blog.name
-	except ValueError:
-		raise Http404
+	posts = Post.objects.filter(blog = _blog).order_by("-published_on")[:3]
+	context["posts"] = posts
+	context["blog_title"] = _blog.title
 
 	return render(request, template, context)
 	
